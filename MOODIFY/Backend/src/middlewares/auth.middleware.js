bbc6e26ccadc6ba5bb0dfb800385d786110309bb
Sandeepsahu18 <editingking998@gmail.com>
2026-04-05 20:03:29 +0530

    feat: initialize backend with user authentication
    - Added package.json for backend dependencies and scripts.
    - Created server.js to set up the Express server and connect to the database.
    - Implemented app.js to configure middleware and routes.
    - Established database connection logic in database.js.
    - Developed user authentication controller with register, login, getMe, and logout functionalities.
    - Created auth middleware for token verification and blacklist checking.
    - Defined user and blacklist models for MongoDB.
    - Set up authentication routes for user registration, login, fetching user info, and logout.

---
+++ MOODIFY/Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+const blacklistModel = require("../models/blacklist.modal");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+async function authUser(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Token not provided",
+    });
+  }
+
+  const isTokenBlacklinsted = await blacklistModel.findOne({
+    token
+  })
+
+  if(!isTokenBlacklinsted){
+    return res.status(401).json({
+      message:"invalid token"
+    })
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded
+    next()
+  } catch (error) {
+    return res.status(401).json({
+        message:"Invalid token"
+    })
+  }
+}
+
+
+module.exports = {authUser}
